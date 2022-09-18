a910c622d9f1b3a9ba9c2804f848d7c942f76016
bogdanova21 <lika.salvatore2015@yandex.ru>
2022-09-18 13:13:30 +0300

    feat(cpp):udpate namecpp

---
+++ 03_cpp/intro/name.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char name[15];
+
+    cout << "What's your name?" <<"\n";
+    cin >> name;
+    cout << "Hello, " << name;
+
+    return 0;
+}
